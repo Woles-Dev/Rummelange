470d6e8a35877afc64cce3eadd42d077be93186a
joackwolf <joacow88@gmail.com>
2023-07-31 23:07:00 -0300

    Navbar y Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import NavBar from './views/Navbar';
 import Proximamente from './views/Proximamente';
+import Footer from './views/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      
       <Proximamente/>
+
+      <Footer/>
     </div>
   );
 }
--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -4,23 +4,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
     return (
     <header class="header">
-		<h1 class="logo"><a href="#">Rumme<span>lange</span></a></h1>
+		<h1 class="logo"><Link to="#">Rumme<span>lange</span></Link></h1>
         <div class="search">
             <input type="text" placeholder="Buscar productos" class="searchInput"/> 
-            <FontAwesomeIcon icon={faMagnifyingGlass} class="searchIcon"/>      
+            <Link to="#"><FontAwesomeIcon icon={faMagnifyingGlass} class="searchIcon"/></Link>
         </div>
         <ul class="main-nav">
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Local</a></li>
-            <li><a href="#">Login</a></li>
-            <li><a href="#"> <FontAwesomeIcon icon={faCartShopping} /> </a></li>
+            <li><Link to="#">Inicio</Link></li>
+            <li><Link to="#">Local</Link></li>
+            <li><Link to="#">Login</Link></li>
+            <li><Link to="#"> <FontAwesomeIcon icon={faCartShopping} /> </Link></li>
         </ul>
         <ul class="bars">
-        <li><a href="#"> <FontAwesomeIcon icon={faBars} /> </a></li>
+        <li><Link to="#"> <FontAwesomeIcon icon={faBars} /> </Link></li>
         </ul>
 	</header> 
     )
