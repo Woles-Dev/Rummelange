03349984d61f0ebc42b5e46db2e38c4ce2b1859b
joackwolf <joacow88@gmail.com>
2023-08-03 22:08:03 -0300

    Sprint1 ii

--- src/views/Main.js
+++ src/views/Main.js
@@ -9,7 +9,7 @@ function Main() {
     <main>
         <div class="product-container">
             <img src={logo} alt="img-product" class="img-product" />
-            <div class="overlay">
+             <div class="overlay">
                 <div class = "items"></div>
                     <div class = "items head">
                         <p>Flower Embroidery Hoop Art</p>
@@ -24,143 +24,7 @@ function Main() {
                 </div>
             </div>
         </div>
-
-        <div class="product-container">
-            <div class="overlay">
-                <div class = "items"></div>
-                    <div class = "items head">
-                        <p>Flower Embroidery Hoop Art</p>
-                    </div>
-                <div class = "items price">
-                    <p class="old">$699</p>
-                    <p class="new">$345</p>
-                </div>
-                <div class="items cart">
-                    <FontAwesomeIcon icon={faCartShopping} class="cart-i"/>
-                    <span>ADD TO CART</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="product-container">
-            <div class="overlay">
-                <div class = "items"></div>
-                    <div class = "items head">
-                        <p>Flower Embroidery Hoop Art</p>
-                    </div>
-                <div class = "items price">
-                    <p class="old">$699</p>
-                    <p class="new">$345</p>
-                </div>
-                <div class="items cart">
-                    <FontAwesomeIcon icon={faCartShopping} class="cart-i"/>
-                    <span>ADD TO CART</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="product-container">
-            <div class="overlay">
-                <div class = "items"></div>
-                    <div class = "items head">
-                        <p>Flower Embroidery Hoop Art</p>
-                    </div>
-                <div class = "items price">
-                    <p class="old">$699</p>
-                    <p class="new">$345</p>
-                </div>
-                <div class="items cart">
-                    <FontAwesomeIcon icon={faCartShopping} class="cart-i"/>
-                    <span>ADD TO CART</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="product-container">
-            <div class="overlay">
-                <div class = "items"></div>
-                    <div class = "items head">
-                        <p>Flower Embroidery Hoop Art</p>
-                    </div>
-                <div class = "items price">
-                    <p class="old">$699</p>
-                    <p class="new">$345</p>
-                </div>
-                <div class="items cart">
-                    <FontAwesomeIcon icon={faCartShopping} class="cart-i"/>
-                    <span>ADD TO CART</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="product-container">
-            <div class="overlay">
-                <div class = "items"></div>
-                    <div class = "items head">
-                        <p>Flower Embroidery Hoop Art</p>
-                    </div>
-                <div class = "items price">
-                    <p class="old">$699</p>
-                    <p class="new">$345</p>
-                </div>
-                <div class="items cart">
-                    <FontAwesomeIcon icon={faCartShopping} class="cart-i"/>
-                    <span>ADD TO CART</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="product-container">
         
-            <div class="overlay">
-                <div class = "items"></div>
-                    <div class = "items head">
-                        <p>Flower Embroidery Hoop Art</p>
-                    </div>
-                <div class = "items price">
-                    <p class="old">$699</p>
-                    <p class="new">$345</p>
-                </div>
-                <div class="items cart">
-                    <FontAwesomeIcon icon={faCartShopping} class="cart-i"/>
-                    <span>ADD TO CART</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="product-container">
-            <div class="overlay">
-                <div class = "items"></div>
-                    <div class = "items head">
-                        <p>Flower Embroidery Hoop Art</p>
-                    </div>
-                <div class = "items price">
-                    <p class="old">$699</p>
-                    <p class="new">$345</p>
-                </div>
-                <div class="items cart">
-                    <FontAwesomeIcon icon={faCartShopping} class="cart-i"/>
-                    <span>ADD TO CART</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="product-container">
-            <div class="overlay">
-                <div class = "items"></div>
-                    <div class = "items head">
-                        <p>Flower Embroidery Hoop Art</p>
-                    </div>
-                <div class = "items price">
-                    <p class="old">$699</p>
-                    <p class="new">$345</p>
-                </div>
-                <div class="items cart">
-                    <FontAwesomeIcon icon={faCartShopping} class="cart-i"/>
-                    <span>ADD TO CART</span>
-                </div>
-            </div>
-        </div>
     </main>
     )
 }
