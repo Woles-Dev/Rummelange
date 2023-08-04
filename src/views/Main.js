042de080f7e3b3b1fffffbe881732133a240f593
fedenilles <fedenilles@gmail.com>
2023-08-03 23:26:46 -0300

    sidebar and Product

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,30 +1,14 @@
 import React from "react";
 import '../Assets/css/main.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-import logo from '../logo.svg';
+import Sidebar from "./components/Sidebar";
+import Products from "./components/Products";
+
 
 function Main() {
     return (
     <main>
-        <div class="product-container">
-            <img src={logo} alt="img-product" class="img-product" />
-             <div class="overlay">
-                <div class = "items"></div>
-                    <div class = "items head">
-                        <p>Flower Embroidery Hoop Art</p>
-                    </div>
-                <div class = "items price">
-                    <p class="old">$699</p>
-                    <p class="new">$345</p>
-                </div>
-                <div class="items cart">
-                    <FontAwesomeIcon icon={faCartShopping} class="cart-i"/>
-                    <span>ADD TO CART</span>
-                </div>
-            </div>
-        </div>
-        
+        <Sidebar/>
+        <Products/>
     </main>
     )
 }
