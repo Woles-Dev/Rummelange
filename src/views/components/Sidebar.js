abdb955c2e8f85a2781bb1d6702a4078b8dd72d0
joackwolf <joacow88@gmail.com>
2023-08-05 00:41:34 -0300

    sidebar joaco

--- src/views/components/Sidebar.js
+++ src/views/components/Sidebar.js
@@ -6,23 +6,15 @@ import "../../Assets/css/Sidebar.css"
 
 function Sidebar() {
     return (
-        <div class="sidebar-container">
-            <ul class="side-nav">
-            <li><Link to="#">Muebles</Link></li>
-            <li><Link to="#">Espejos</Link></li>
-            <li><Link to="#">Cortinas</Link></li>
-            <li className="sub-list"><Link to="#">Cesto</Link>
-                <ul className="sub-sidebar">
-                <li><Link to="#">Cestos Basura</Link></li>
-                <li><Link to="#">Cestos Guardado</Link></li>
-                </ul>
-            </li>
-            <li>
-                
-            </li>
-
-            </ul>    
-        </div>
+        <aside class="sidebar">
+        <ul class="side-list">
+            <li class="link"><Link to="#"> Cestos</Link></li>
+            <li class="link"><Link to="#"> Espejos</Link></li>
+            <li class="link"><Link to="#"> Cortinas</Link></li>
+            <li class="link"><Link to="#"> Toallas </Link></li>
+            <li class="link"><Link to="#"> Aromaticos </Link></li>
+        </ul>
+    </aside>
     )
 }
 
