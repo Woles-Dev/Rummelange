58800d93f57e46f6279df533fcc905768393e64c
joackwolf <joacow88@gmail.com>
2023-08-24 22:42:15 -0300

    falta mejorar visual1

--- src/views/components/subcomponents/AddToCart.js
+++ src/views/components/subcomponents/AddToCart.js
@@ -6,7 +6,13 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 function AddToCart() {
     return (
-        <button class="add-to-cart-btn">Add to Cart</button>
+        <div class="cart">
+            
+            <button class="add-to-cart-btn">
+            <FontAwesomeIcon icon={faCartShopping} class="cart-i"/> 
+            <p>Add to Cart</p>
+            </button>
+        </div>
     )
 }
 
